bf90fc6ac0a62d135850f36d08ccfdc77283ff89
ShadowSharingan <achar.samarth@gmail.com>
2025-06-25 16:36:09 +0100

    changes

--- DataTypes/Lists/Program2.py
+++ DataTypes/Lists/Program2.py
@@ -1,1 +1,11 @@
-lst = [12,35,1,10,34,1]
+lst = [10,10,10,10,10]
+
+def getSecondLargest(lst):
+    lst.sort()
+    n = len(lst)
+    for i in range(n-2,-1,-1):
+        if lst[i] != lst[-1]:
+            return lst[i]
+    return -1
+
+print(getSecondLargest(lst))
