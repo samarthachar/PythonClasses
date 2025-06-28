235f08d9ec4636ecc0e7eb5cf3497e28159da2fa
ShadowSharingan <achar.samarth@gmail.com>
2025-06-28 19:30:47 +0100

    Done

--- DataTypes/Lists/homework(25-06).py
+++ DataTypes/Lists/homework(25-06).py
@@ -1,3 +1,23 @@
 lst = [1,2,3,0,5,4,0]
 output = [1,2,3,4,5,0,0]
 
+
+# lst.sort()
+
+# def getZero():
+#     index = 0
+#     if lst[index] == 0:
+#         del lst[index]
+#         lst.append(0)
+#         index+= 1
+#         getZero()
+
+# getZero()
+# print(lst)
+index = 0
+lst.sort()
+for i in lst:
+    if i == 0:
+        del lst[index]
+        lst.append(0)
+    index += 1
