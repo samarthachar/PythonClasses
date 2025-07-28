1c6a4b22471a9466a3225df110a46fb525926693
Samarth <achar.samarth@gmail.com>
2025-07-28 22:17:16 +0100

    Changes Made

---
+++ Practical/28thJuly.py
@@ -0,0 +1,14 @@
+# class Room:
+#     def __init__(self, length, breath):
+#         self.length = length
+#         self.breath = breath
+#     def area(self):
+#         self.area =  self.length * self.breath
+#         return self.area
+# living_room = Room(20, 15)
+# print("Area of the living room is:", living_room.area())
+# kitchen = Room(15, 10)
+# print("Area of the kitchen is:", kitchen.area())
+# study_room = Room(10, 12)
+# print("Area of the study room is:", study_room.area())
+
